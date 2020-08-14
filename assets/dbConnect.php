9230d3ccd17c60e14b99b1c25645bb34bc06c5a7
Kevin Matos <kevmgaming@gmail.com>
2020-08-13 19:21:07 -0600

    database connection working locally

--- assets/dbConnect.php
+++ assets/dbConnect.php
@@ -3,7 +3,11 @@ function get_db()
 {
   $db = NULL;
   try {
-    $dbUrl = getenv('DATABASE_URL');
+    if (getenv('DATABASE_URL')) {
+      $dbUrl = getenv('DATABASE_URL');
+    } else {
+      $dbUrl = "postgres://qzlwtfnyadsvam:bdf4f245cf4d618c9770b3d89913aac28140467284ee9429f6a81bba3e66be5c@ec2-52-204-20-42.compute-1.amazonaws.com:5432/d7qotsi56goomr";
+    }
     $dbOpts = parse_url($dbUrl);
 
     $dbHost = $dbOpts["host"];
@@ -11,7 +15,7 @@ function get_db()
     $dbUser = $dbOpts["user"];
     $dbPassword = $dbOpts["pass"];
     $dbName = ltrim($dbOpts["path"],'/');
-    echo $dbUrl;
+
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -19,6 +23,7 @@ function get_db()
     echo 'Error!: ' . $ex->getMessage();
     die();
   }
+  echo "returning successfully";
   return $db;
 }
 ?>
