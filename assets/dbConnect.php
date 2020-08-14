37a11b3eeb8e23c8ade8412a3f5b93863a50df52
Kevin Matos <kevmgaming@gmail.com>
2020-08-13 18:53:45 -0600

    reveal driver

--- assets/dbConnect.php
+++ assets/dbConnect.php
@@ -11,7 +11,7 @@ function get_db()
     $dbUser = $dbOpts["user"];
     $dbPassword = $dbOpts["pass"];
     $dbName = ltrim($dbOpts["path"],'/');
-
+    echo $dbUrl;
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
